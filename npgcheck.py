82eb9bee6aee639d8cf43cedec2f28d11c5b2b8b
admin <admin@192.168.186.116>
2017-03-02 13:39:43 +0800

    list all file check .npg file add npglist

---
+++ npgcheck.py
@@ -0,0 +1,40 @@
+# coding:utf-8
+import os
+
+
+def dirlist(path, allfile):
+    '''
+        list all file
+    '''
+    try:
+        filelist = os.listdir(path)
+        for filename in filelist:
+            filepath = os.path.join(path, filename)
+            if os.path.isdir(filepath):
+                dirlist(filepath, allfile)
+            else:
+                allfile.append(filepath)
+
+        return allfile
+    except WindowsError:
+        print "WindowsError系统调用失败"
+        pass
+        
+
+
+def ngplist(allfile, ngplist):
+    '''
+        return .npg file
+    '''
+    for file in allfile:
+        if file.endswith('.npg'):
+            ngplist.append(file)
+    return ngplist
+    
+
+if __name__ == '__main__':
+    path = 'c:\\'
+    allfile = dirlist(path, [])
+    print len(allfile)
+    ngplist = ngplist(allfile, [])
+    print ngplist
